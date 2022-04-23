873e23cf4dd3aa8a8a896e57c98ba9426440a2c9
root <a07025425@itesm.mx>
2022-04-22 20:28:31 -0700

    add file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # Create a file
 
 file { '/tmp/school':
-	ensure => 'present',
-	replace => 'no',
-	group => 'www-data',
-	owner => 'www-data',
-	mode => '0744',
-	content => "I love Puppet",
+  ensure  => 'present',
+  replace => 'no',
+  group   => 'www-data',
+  owner   => 'www-data',
+  mode    => '0744',
+  content => "I love Puppet\n",
 }
